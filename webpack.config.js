a6d8a32524a11f8fba4faf3b36065b63064547b5
chenjuanhe <chenjuanhe@pptv.com>
2019-03-13 16:18:06 +0800

    fixed webpack warning

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
+const path = require('path');
+
 module.exports = {
     devtool:'cheap-module-eval-source-map',
+    mode:'development',
     entry: {
         index: './src/index.jsx'
     },
     output: {
-        path: './dist/',
+        path: path.resolve(__dirname, "dist"),
         filename: '[name].js',
         libraryTarget: 'umd'
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /.jsx?$/,
-                loader: 'babel-loader'
+                include:[
+                    path.resolve(__dirname,'src')
+                ],
+                use:['babel-loader']
             }
         ]
-    }
+    },
+    performance: {
+        hints: false,
+        maxAssetSize: 200000,
+        maxEntrypointSize: 400000,
+        assetFilter: function(assetFilename) {
+          return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+        }
+    },
 };
